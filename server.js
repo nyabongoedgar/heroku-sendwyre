215066de006956a8a583a8024fc2ec3327e421d7
nyabongoedgar <nyabongoedgar@gmail.com>
2020-05-27 13:15:15 +0300

    modify heroku configuration

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("dist"));
+app.use(express.static("build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
